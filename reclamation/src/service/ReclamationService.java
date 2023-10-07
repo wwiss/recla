63efe54121e24fe50f76f3622625b7367aa39941
wissem <115354891+wwiss@users.noreply.github.com>
2023-10-07 15:14:56 +0100

    Add files via upload

---
+++ reclamation/src/service/ReclamationService.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+
+import entities.Reclamation;
+import utilities.Myconnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public  class ReclamationService implements CrudInterface<Reclamation> {
+
+    private Connection cnx;
+    private Statement st;
+    private ResultSet rs;
+
+    public ReclamationService() {
+        cnx = Myconnection.getInstance().getCnx();
+    }
+
+   @Override
+public void create(Reclamation entity) {
+    PreparedStatement pst = null;
+    int generatedId = -1;
+
+    try {
+        pst = cnx.prepareStatement("INSERT INTO reclamation (numero_mobile, date_traitement, description, objet, status, nom, prenom, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+        pst.setInt(1, entity.getNumero_mobile());
+        pst.setDate(2, entity.getDate_traitement());
+        pst.setString(3, entity.getDescription());
+        pst.setString(4, entity.getObjet());
+        pst.setString(5, entity.getStatus());
+        pst.setString(6, entity.getNom());
+        pst.setString(7, entity.getPrenom());
+        pst.setString(8, entity.getEmail());
+
+        int n = pst.executeUpdate();
+        if (n > 0) {
+            ResultSet generatedKeys = pst.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                generatedId = generatedKeys.getInt(1); // Get the generated ID
+            }
+        }
+        entity.setId(generatedId);
+    } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+        // Handle the exception as needed for your application
+    } finally {
+        try {
+            if (pst != null) {
+                pst.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle closing error if necessary
+        }
+    }
+}
+
+    public PreparedStatement prepareStatement(String sql) throws SQLException {
+        return cnx.prepareStatement(sql);
+    }
+
+    @Override
+    public void update(Reclamation entity) {
+        PreparedStatement pst;
+        int n = 0;
+
+        try {
+           pst = cnx.prepareStatement("INSERT INTO reclamation ( numero_mobile,  date_traitement , description,  objet,  status,  nom,  prenom,  email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            //pst.setInt(1, entity.getId());
+            pst.setInt(2, entity.getNumero_mobile());
+             pst.setDate(3, entity.getDate_traitement());
+             
+            pst.setString(4, entity.getDescription());
+            pst.setString(5, entity.getObjet());
+             pst.setString(6, entity.getStatus());
+          
+            pst.setString(7, entity.getNom());
+            pst.setString(8, entity.getPrenom());
+             pst.setString(9, entity.getEmail());
+
+
+            n = pst.executeUpdate();
+            pst.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+        int n = 0;
+        PreparedStatement st;
+        try {
+
+            st = cnx.prepareStatement("DELETE FROM `reclamation` WHERE `id`=?");
+
+            st.setInt(1, id);
+
+            n = st.executeUpdate();
+            st.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Override
+    public Reclamation getById(int id) {
+
+        PreparedStatement st;
+       Reclamation  reclamation;
+        reclamation = new Reclamation();
+        try {
+
+            st = cnx.prepareStatement("SELECT * FROM `reclamation` WHERE `id`=?");
+
+            st.setInt(1, id);
+
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+
+               reclamation.setId(rs.getInt("id"));
+                reclamation.setNumero_mobile(rs.getInt("numero_mobile"));
+                reclamation.setDate_traitement(rs.getDate("date_traitement"));
+                reclamation.setDescription(rs.getString("description"));
+                reclamation.setObjet(rs.getString("objet"));
+               reclamation.setStatus(rs.getString("status"));
+                reclamation.setNom(rs.getString("nom"));
+                reclamation.setPrenom(rs.getString("prenom"));
+                reclamation.setEmail(rs.getString("email"));
+               
+
+            }
+            cnx.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return reclamation;
+    }
+
+    @Override
+public List<Reclamation> getAll() {
+    List<Reclamation> reclamationList = new ArrayList<>();
+    PreparedStatement st;
+
+    try {
+        st = cnx.prepareStatement("SELECT * FROM reclamation");
+        ResultSet rs = st.executeQuery();
+
+        while (rs.next()) {
+            Reclamation reclamation = new Reclamation();
+            reclamation.setId(rs.getInt("id")); // Include the ID field
+            reclamation.setNumero_mobile(rs.getInt("numero_mobile"));
+            reclamation.setDate_traitement(rs.getDate("date_traitement"));
+            reclamation.setDescription(rs.getString("description"));
+            reclamation.setObjet(rs.getString("objet"));
+            reclamation.setStatus(rs.getString("status"));
+            reclamation.setNom(rs.getString("nom"));
+            reclamation.setPrenom(rs.getString("prenom"));
+            reclamation.setEmail(rs.getString("email"));
+
+            reclamationList.add(reclamation);
+        }
+
+        st.close();
+    } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+    }
+
+    return reclamationList;
+}
+
+}
---
+++ reclamation/src/test/Main.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import entities.Reclamation;
+import service.ReclamationService;
+import utilities.Myconnection;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author ZARAMI
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Create a connection to the database
+            Myconnection myconnection = Myconnection.getInstance();
+
+            // Get the current date
+            Date currentDate = new Date(System.currentTimeMillis());
+
+            // Create a new Reclamation instance
+            Reclamation reclamation = new Reclamation();
+
+            // Set the Reclamation object's properties
+            // Note: You may want to remove the manual setting of the ID to allow the database to generate it.
+            reclamation.setNumero_mobile(2222222);
+            reclamation.setDate_traitement(currentDate);
+            reclamation.setDescription("ddddd");
+            reclamation.setObjet("ddddd");
+            reclamation.setStatus("fffff");
+            reclamation.setNom("yyyyy");
+            reclamation.setPrenom("Med xx");
+            reclamation.setEmail("mxxxi@esprit.tn");
+
+            // Create a ReclamationService instance and use it to insert the record
+            ReclamationService rr = new ReclamationService();
+           // rr.create(reclamation);
+           // rr.delete(2);
+           // System.out.println(rr.getById(3).toString());
+       
+       List<Reclamation> reclamationList = new ArrayList<>();
+         reclamationList =rr.getAll();
+
+        for (Reclamation recalmation : reclamationList) {
+            System.out.println(reclamation);
+        }
+            // Print the Reclamation object
+           // System.out.println(reclamation);
+
+            // Close the database connection when done
+       
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle any exceptions that occur during database operations
+        }
+    }
+}
